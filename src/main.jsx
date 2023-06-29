074a8784dc063a9809ed7fb01fbbafe68fc374d1
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-06-29 11:45:13 -0400

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root, { loader as rootLoader } from "./routes/root";
+import ErrorPage from "./ErrorPage";
+import Contact from "./routes/contact";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        loader: rootLoader,
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact />,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
